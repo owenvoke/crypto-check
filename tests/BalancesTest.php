9eab17add1cda337f850d12b6fad430776bc70b4
pxgamer <owzie123@gmail.com>
2017-11-13 11:03:32 +0000

    Added BalancesTest class

---
+++ tests/BalancesTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace pxgamer\CryptoCheck;
+
+use PHPUnit\Framework\TestCase;
+use pxgamer\CryptoCheck\Exceptions;
+
+/**
+ * Class BalancesTest
+ */
+class BalancesTest extends TestCase
+{
+    /**
+     * Test that the balance can be fetched for wallets.
+     * @throws \Exception
+     */
+    public function testCanFetchBalances()
+    {
+        $balances = Balances::fetch();
+
+        $this->assertInternalType('array', $balances);
+    }
+
+    /**
+     * Test that the fetch() method throws an exception on an invalid coin type.
+     * @throws \Exception
+     */
+    public function testThrowExceptionOnInvalidCoinType()
+    {
+        $this->expectException(Exceptions\InvalidWalletTypeException::class);
+        Balances::fetch('Botchain');
+    }
+
+    /**
+     * Test that the fetch() method throws an exception on an invalid Bitcoin address.
+     * @throws \Exception
+     */
+    public function testThrowExceptionOnInvalidBitcoinAddress()
+    {
+        $this->expectException(Exceptions\WalletNotFoundException::class);
+        Balances::fetch(Wallet::BITCOIN, WalletTest::INVALID_ADDRESS);
+    }
+
+    /**
+     * Test that the fetch() method throws an exception on an invalid Ethereum address.
+     * @throws \Exception
+     */
+    public function testThrowExceptionOnInvalidEthereumAddress()
+    {
+        $this->expectException(Exceptions\WalletNotFoundException::class);
+        Balances::fetch(Wallet::ETHEREUM, WalletTest::INVALID_ADDRESS);
+    }
+
+    /**
+     * Test that the fetch() method throws an exception on an invalid Dash address.
+     * @throws \Exception
+     */
+    public function testThrowExceptionOnInvalidDashAddress()
+    {
+        $this->expectException(Exceptions\WalletNotFoundException::class);
+        Balances::fetch(Wallet::DASH, WalletTest::INVALID_ADDRESS);
+    }
+}
